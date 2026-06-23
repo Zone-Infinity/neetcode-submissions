17ad4dc8b2c44c066282fdb5ce43bed6d3d8ad88
Soham Rajendra Dokhale <68643247+Zone-Infinity@users.noreply.github.com>
2026-06-23 15:55:01 +0530

    Add: valid-parenthesis-string - submission-1

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-1.cpp
@@ -0,0 +1,31 @@
+class Solution {
+   public:
+    bool checkValidString(string s) {
+        stack<int> left;
+        stack<int> star;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') {
+                left.push(i);
+            } else if (s[i] == '*') {
+                star.push(i);
+            } else {
+                if (!left.empty()) {
+                    left.pop();
+                } else if (!star.empty()) {
+                    star.pop();
+                } else
+                    return false;
+            }
+        }
+
+        while (!left.empty() && !star.empty() && left.top() < star.top()) {
+            left.pop();
+            star.pop();
+        }
+
+        if (!left.empty()) return false;
+
+        return true;
+    }
+};
