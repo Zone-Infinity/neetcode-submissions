7ba7a153abf6043d33e176dd1d279c397ef9219f
Soham Rajendra Dokhale <68643247+Zone-Infinity@users.noreply.github.com>
2026-06-23 14:27:51 +0530

    Add: pow-x-n - submission-1

---
+++ Data Structures & Algorithms/pow-x-n/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+   public:
+    double myPow(double x, int n) {
+        if (x == 0) return 0;
+
+        if (n == 0) return 1;
+        if (n == 1) return x;
+        if (n == -1) return 1 / x;
+
+        double half = myPow(x, n / 2);
+        if (n % 2 == 0) return half * half;
+        return half * half * (n > 0 ? x : 1 / x);
+    }
+};
